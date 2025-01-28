3c173bd0cb16f242c180792b4d5e3ba6de3d445e
jsloan3 <jaxon.sloan@ucalgary.ca>
2025-01-28 15:30:53 -0700

    more fix

--- main.py
+++ main.py
@@ -167,13 +167,13 @@ async def on_ready():
 @client.event
 async def on_voice_state_update(member, state_before, state_after):
     global current_voice
-    print(f"voice state change: m: {member} s_b: {state_before} s_a: {state_after}")
+    print(f"voice state change: m: {member} s_b: {state_before.channel.name} s_a: {state_after}")
     print(f"current voice channel = {current_voice.channel}")
     if current_voice == None:
         return
     if not (state_before != None and state_after == None):
         return
-    if state_before.channel == current_voice.channel:
+    if state_before.channel.name == current_voice.channel:
         current_voice.disconnect()
     
 
